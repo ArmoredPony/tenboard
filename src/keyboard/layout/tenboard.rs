8582ed8356ce54d520377ccd71f081fe01dfa9bc
ArmoredPony <raxmetov2003@gmail.com>
2024-08-23 10:25:11 +0400

    add `TenboardThumbConstrained`

--- src/keyboard/layout/tenboard.rs
+++ src/keyboard/layout/tenboard.rs
@@ -1,9 +1,13 @@
 //! Describes Tenboard keyboard layout.
 
-use rand::prelude::*;
 use std::collections::HashMap;
 
-use crate::keyboard::{hands::HandsState, Keyboard, NoSuchChar, TYPABLE_SYMBOLS};
+use rand::prelude::*;
+
+use crate::keyboard::{
+  hands::HandsState, Keyboard, NoSuchChar, DIGIT_CHARS, LOWERCASE_CHARS,
+  PUNCTUATION_CHARS, TYPABLE_CHARS,
+};
 
 pub trait Tenboard: Keyboard {
   /// Creates a new Tenboard keyboard layout where each character
@@ -42,14 +46,42 @@ pub struct TenboardUnconstrained {
 
 impl Tenboard for TenboardUnconstrained {
   fn new_random() -> Self {
-    let mut handsstates: Vec<_> = HandsState::iterate_unique().collect();
+    let mut handsstates: Vec<_> =
+      HandsState::iterate_one_two_key_all_states().collect();
     handsstates.shuffle(&mut rand::thread_rng());
     Self {
-      layout: HashMap::from_iter(TYPABLE_SYMBOLS.chars().zip(handsstates)),
+      layout: HashMap::from_iter(TYPABLE_CHARS.chars().zip(handsstates)),
     }
   }
 
   fn try_type_char(&mut self, ch: char) -> Result<HandsState, NoSuchChar> {
     self.layout.get(&ch).copied().ok_or(NoSuchChar { ch })
+  }
+}
+
+/// Constrained Tenboard layout.
+/// 'whitespace' and 'enter' are bound to single key thumb chords.
+pub struct TenboardThumbConstrained {
+  layout: HashMap<char, HandsState>,
+}
+
+impl Tenboard for TenboardThumbConstrained {
+  fn new_random() -> Self {
+    let mut handsstates: Vec<_> =
+      HandsState::iterate_one_two_key_with_thumbs().collect();
+    handsstates.shuffle(&mut rand::thread_rng());
+    let chars_iter =
+      TYPABLE_CHARS.chars().filter(|&ch| ch != ' ' && ch != '\n');
+    Self {
+      layout: HashMap::from_iter(chars_iter.zip(handsstates)),
+    }
+  }
+
+  fn try_type_char(&mut self, ch: char) -> Result<HandsState, NoSuchChar> {
+    match ch {
+      ' ' => Ok(HandsState::left_thumb()),
+      '\n' => Ok(HandsState::right_thumb()),
+      _ => self.layout.get(&ch).ok_or(NoSuchChar { ch }).copied(),
+    }
   }
 }
